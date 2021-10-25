3facffefe177934d3ae7eb3da3a0afe4bb826080
AntoniKania <antek.kania@wp.pl>
2021-10-25 18:10:33 +0200

    test

---
+++ test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                double cos;
+                Console.WriteLine("Wpisz liczbe ;)");
+                Console.Write(": ");
+                double.TryParse(Console.ReadLine(), out cos);
+                Console.WriteLine(cos);
+                
+
+            }
+        }
+    }
+}
