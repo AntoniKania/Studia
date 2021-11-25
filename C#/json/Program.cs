306fce87f65b467dd27ecf0a3d1687c5876ecf00
AntoniKania <antek.kania@wp.pl>
2021-11-25 11:43:38 +0100

    json

---
+++ C#/json/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+//using Models;
+
+// dotnet add package NuGet.CommandLine --version 5.8.0
+// dotnet add package Newtonsoft.Json --version 13.0.1
+
+namespace JsonSample
+{
+    class Testc
+    {
+        static void Main(string[] args)
+        {
+            string data = File.ReadAllText("./hero.json");
+            JObject Hero = JObject.Parse(data);
+            Console.WriteLine("Name: " + Hero["Name"]);
+            Console.WriteLine("Strength: " + Hero["Strength"]);
+            Console.WriteLine("Dexterity: " + Hero["Dexterity"]);
+            Console.WriteLine("Intelligence: " + Hero["Intelligence"]);
+            Console.WriteLine("HP: " + Hero["HP"]);
+            Console.WriteLine("MP: " + Hero["MP"]);
+            Console.Write("Array: ");
+            foreach (string tmp in Hero["Array"])
+            {
+                Console.Write(tmp + ", ");
+            }
+            //Console.WriteLine(Hero.ToString());
+
+            Hero["Strength"] = (int)Hero["Strength"] + 12;
+            Hero["Dexterity"] = (int)Hero["Dexterity"] + 6;
+
+            File.WriteAllText("./hero2.json", Hero.ToString());
+        }
+    }
+}
